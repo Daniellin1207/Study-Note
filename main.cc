2492fbf0b3a7ea4e001599d24c24e96318abab68
SENSETIME\linyili <linyili@sensetime.com>
2019-12-24 14:22:51 +0800

    recommit first

--- main.cc
+++ main.cc
@@ -1,35 +1,15 @@
-// #include <iostream>
-// using namespace std;
-// int main(){
-//     cout<<"hello"<<299<<endl;
-//     return 0;
-// }
-
-
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
-#include <iostream>
- 
-#include "http.h"
- 
+#include<iostream>
 using namespace std;
- 
-int main(int argc, char *argv[])
-{
-  CurlHttp curl_http;
-  string str_url = "https://www.baidu.com";  // 地址、端口号
-  string str_test = "Hello World!";
- 
-  while (1) {
-    cout << "Post data ...\n";
-    
-    string result;
-    int res = curl_http.http_get(str_url.c_str(),&result);
-    cout << "[Response]: " << result << '\n';
-    
-    sleep(1);
-  }
-  
+#include "math_function.cc"
+int main(){
+  cout<<"first input"<<endl;
+  cout<<"second input"<<endl;
+  cout<<"third input"<<endl;
+  cout<<"fourth input"<<endl;
+  int a=10;
+  int b=2.7;
+  cout<<"a: "<<a<<" b: "<<b<<endl;
+  cout<<"sum: "<<Add(a,b)<<endl;
+  cout<<"multi: "<<Multi(a,b)<<endl;
   return 0;
 }
---
+++ math_function.cc
@@ -0,0 +1,10 @@
+#include <math.h>
+
+template<class T>
+T Add(T a, T b){
+    return a+b;
+}
+template<class T>
+float Multi(T a,T b){
+    return float(a*b);
+}
