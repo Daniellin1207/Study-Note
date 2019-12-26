a4fb8856151c56c9d8e83599f0bd2dca11129959
SENSETIME\linyili <linyili@sensetime.com>
2019-12-26 10:36:02 +0800

    add debug setting in Cmake

--- Math/math_function.cc
+++ Math/math_function.cc
@@ -8,3 +8,18 @@ template<class T>
 float Multi(T a,T b){
     return float(a*b);
 }
+
+template<typename T,typename U>
+class Num{
+public:
+    T value;
+    auto hello(T a,U b)->decltype(a+b);
+};
+
+template<typename T,typename U>
+auto Num<T,U>::hello(T a,U b)->decltype(a+b){
+    return a+b;
+}
+// float Num<T,U>::hello(T a,U b){
+//     return a+b;
+// }
--- main.cc
+++ main.cc
@@ -1,45 +1,53 @@
-// #include<iostream>
-// using namespace std;
-// #include "Math/math_function.cc"
-// int main(){
-//   cout<<"first input"<<endl;
-//   cout<<"second input"<<endl;
-//   cout<<"third input"<<endl;
-//   cout<<"fourth input"<<endl;
-//   int a=10;
-//   int b=2.7;
-//   cout<<"a: "<<a<<" b: "<<b<<endl;
-//   cout<<"sum: "<<Add(a,b)<<endl;
-//   cout<<"multi: "<<Multi(a,b)<<endl;
-//   return 0;
-// }
-
-
-
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
-#include <iostream>
- 
-#include "http.h"
- 
+#include<iostream>
 using namespace std;
- 
-int main(int argc, char *argv[])
+// #include "Math/math_function.cc"
+
+void print(){
+  cout<<"hello daniel cpp"<<endl;
+}
+
+int max(int a,int b)
 {
-  CurlHttp curl_http;
-  string str_url = "https://www.baidu.com";  // 地址、端口号
-  string str_test = "Hello World!";
- 
-  while (1) {
-    cout << "Post data ...\n";
-    
-    string result;
-    int res = curl_http.http_get(str_url.c_str(),&result);
-    cout << "[Response]: " << result << '\n';
-    
-    sleep(1);
-  }
-  
+    return a>b?a:b;
+}
+
+
+
+int main(){
+  cout<<"first input"<<endl;
+  cout<<"second input"<<endl;
+  cout<<"third input"<<endl;
+  cout<<"fourth input"<<endl;
+  int a=10;
+  int b=2.7;
+  cout<<"a: "<<a<<" b: "<<b<<endl;
+  // cout<<"sum: "<<Add(a,b)<<endl;
+  // cout<<"multi: "<<Multi(a,b)<<endl;
   return 0;
 }
+
+
+// #include <stdio.h>
+// #include <string.h>
+// #include <time.h>
+// #include <iostream>
+// #include "http.h"
+// using namespace std;
+// int main(int argc, char *argv[])
+// {
+//   cout<<"hello\n";
+//   cout<<"daniel\n";
+//   CurlHttp curl_http;
+//   string str_url = "https://www.baidu.com";  // 地址、端口号
+//   string str_test = "Hello World!";
+ 
+//   while (1) {
+//     cout << "Post data ...\n";
+//     string result;
+//     int res = curl_http.http_get(str_url.c_str(),&result);
+//     cout << "[Response]: " << result << '\n';
+//     sleep(1);
+//   }
+  
+//   return 0;
+// }
